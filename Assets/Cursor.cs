01aa3c534b2ab2da8bbb17cab85dae9d982caa08
Corey Schaf <cschaf@gmail.com>
2018-02-20 00:04:44 -0500

    raycasting on layer items

---
+++ Assets/Cursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    CameraRaycaster cameraRayCaster;
+
+	// Use this for initialization
+	void Start () 
+    {
+        cameraRayCaster = GetComponent<CameraRaycaster>();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        print(cameraRayCaster.LayerHit);
+	}
+}
