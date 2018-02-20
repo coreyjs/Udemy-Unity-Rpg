7f0543bebae6c8b9a214dbadc50e8edf6d557d60
Corey Schaf <cschaf@gmail.com>
2018-02-20 00:30:11 -0500

    imported palyer movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+        
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
+            currentClickTarget = cameraRaycaster.hit.point;  // So not set in default case
+        }
+        m_Character.Move(currentClickTarget - transform.position, false, false);
+    }
+}
+
