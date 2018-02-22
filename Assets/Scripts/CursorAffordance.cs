9c661fb9bc4b0e168d31a91ce6c4f82ada319c94
Corey Schaf <cschaf@gmail.com>
2018-02-22 13:04:08 -0500

    refactoring, folders, curosor art

--- Assets/Scripts/CursorAffordance.cs
+++ Assets/Scripts/CursorAffordance.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cursor : MonoBehaviour {
+public class CursorAffordance : MonoBehaviour {
 
     CameraRaycaster cameraRayCaster;
 
