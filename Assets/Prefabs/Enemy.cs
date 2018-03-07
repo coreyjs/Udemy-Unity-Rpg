40f9493ca6c3130851febf398e80678a1b84b849
Corey Schaf <cschaf@gmail.com>
2018-03-07 00:42:44 -0500

    enemy health bars

---
+++ Assets/Prefabs/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] private float maxHealthPoints = 100f;
+    private float currentHealthPoints = 100f;
+
+    public float healthAsPercentage
+    {
+        get { return currentHealthPoints /  maxHealthPoints;  }
+    }
+
+}
