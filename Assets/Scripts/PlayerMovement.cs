29e4b43eb2ae8c2e6cb4779d009c44677277eb82
Corey Schaf <cschaf@gmail.com>
2018-03-24 14:18:44 -0400

    Player movement set to use dynamic AI with terrain nav mesh

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,17 +11,46 @@ public class PlayerMovement : MonoBehaviour
 {
     bool isInDirectMode = false;
 
-    ThirdPersonCharacter thirdPersonCharacterCharacter;   // A reference to the ThirdPersonCharacter on the object
-    CameraRaycaster cameraRaycaster;
-    Vector3 currentDestination, clickPoint;
-        
-    
+    ThirdPersonCharacter thirdPersonCharacterCharacter = null;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster = null;
+    Vector3 currentDestination, clickPoint; 
+    AICharacterControl aiCharacterControl = null;
+
+    [SerializeField] const int walkableLayerNumber = 8;
+    [SerializeField] const int enemyLayerNumber = 9;
+
+    GameObject walkTarget = null;
 
     void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
         thirdPersonCharacterCharacter = GetComponent<ThirdPersonCharacter>();
         currentDestination = transform.position;
+        aiCharacterControl = GetComponent<AICharacterControl>();
+
+        walkTarget = new GameObject("WalkTarget");
+
+        cameraRaycaster.notifyMouseClickObservers += ProcessMouseClick;
+    }
+
+    void ProcessMouseClick(RaycastHit raycastHit, int layerHit)
+    {
+        switch (layerHit)
+        {
+            case enemyLayerNumber:
+                // navigatate to the enemy
+                GameObject enemy = raycastHit.collider.gameObject;
+                aiCharacterControl.SetTarget(enemy.transform);
+                break;
+            case walkableLayerNumber:
+                // navigate to a point on the ground
+                walkTarget.transform.position = raycastHit.point;
+                aiCharacterControl.SetTarget(walkTarget.transform);
+                break;
+            default:
+                Debug.LogError("Dont know hjow to handle mouse click or player movement");
+                return;
+        }
     }
 
     // TODO Make this get called again
