3a3558b055016e90eaa551d4a68812528e4aeb32
Corey Schaf <cschaf@gmail.com>
2018-04-15 14:28:58 -0400

    enemy move and attack radius

--- Assets/Prefabs/Enemy.cs
+++ Assets/Prefabs/Enemy.cs
@@ -9,6 +9,7 @@ public class Enemy : MonoBehaviour, IDamageable
 {
     [SerializeField] private float maxHealthPoints = 100f;
     [SerializeField] private float attackRadius = 4f;
+    [SerializeField] float chaseRadius = 6f;
 
     private float currentHealthPoints = 100f;
 
@@ -33,6 +34,12 @@ public class Enemy : MonoBehaviour, IDamageable
         float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
         if (distanceToPlayer <= attackRadius)
         {
+           print(gameObject.name + " attacking player");
+           // TODO spawn projecticles
+        }
+
+         if (distanceToPlayer <= chaseRadius)
+        {
             aiCharacterControl.SetTarget(player.transform);
         }
         else
@@ -44,6 +51,19 @@ public class Enemy : MonoBehaviour, IDamageable
     public void TakeDamage(float damage)
     {
         currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw attack spehere
+        Gizmos.color = new Color(255f, 0f, 0, .5f);
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+        // Draw move spehere
+        Gizmos.color = new Color(0f, 0f, 255f, .5f);
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
+
+        
     }
 
 }
