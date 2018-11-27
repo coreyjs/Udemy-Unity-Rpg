a64e2a0e72f2b46a19fc27385f57212aa20f16b7
Corey Schaf <cschaf@gmail.com>
2018-11-26 23:15:21 -0500

    This adds multiple weapon support, hand locations and grip locations on weapons.

---
+++ Assets/Characters/DominantHand.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DominantHand : MonoBehaviour {
+	// Add to dominant hand on character prefab
+	
+}
--- Assets/Prefabs/Scripts/Player.cs
+++ Assets/Prefabs/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour, IDamageable
 {
@@ -12,6 +13,7 @@ public class Player : MonoBehaviour, IDamageable
     [SerializeField] float minTimeBetweenHits = 0.5f;
     [SerializeField] float maxAttackRange =2f;
     [SerializeField] Weapon weaponInUse;
+
 
     GameObject currentTarget;
     private float currentHealthPoints;
@@ -29,8 +31,18 @@ public class Player : MonoBehaviour, IDamageable
     private void PutWeaponInHand()
     {
         var weaponPrefab = weaponInUse.GetWeaponPrefab();
-        var weapon = Instantiate(weaponPrefab);
-        // TODO Move to correct place and child to hand
+        GameObject dominantHand = RequestDominantHand();
+        var weapon = Instantiate(weaponPrefab, dominantHand.transform);
+        weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
+        weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+    }
+
+    private GameObject RequestDominantHand() {
+        var dominantHands = GetComponentsInChildren<DominantHand>();
+        int numberOfDominantHands = dominantHands.Length;
+        Assert.AreNotEqual(numberOfDominantHands, 0, "No DominantHand found on player, please add one");
+        Assert.IsFalse(numberOfDominantHands > 1, "Multiple dominant hand scripts on Player, please remove one");
+        return dominantHands[0].gameObject;
     }
 
     private void RegisterForMouseClick()
